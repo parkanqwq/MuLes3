c8d665eaed7245e44a354a9c082c1d09ffd7e6b5
Maksim Kalabukhov <bfmaximmm1992@gmail.com>
2021-12-08 21:10:39 +0300

    3 less

--- app/src/main/java/com/kalabukhov/app/mules3/MainActivity.kt
+++ app/src/main/java/com/kalabukhov/app/mules3/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.kalabukhov.app.mules3
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kalabukhov.app.mules3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigationShits()
+    }
+
+    private fun navigationShits() {
+        binding.bottomNavigationMenu.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    binding.fragmentContainer.setBackgroundColor(Color.RED)
+                    true
+                }
+                R.id.page_2 -> {
+                    binding.fragmentContainer.setBackgroundColor(Color.BLUE)
+                    true
+                }
+                else -> {
+                    binding.fragmentContainer.setBackgroundColor(Color.RED)
+                    true
+                }
+            }
+        }
     }
 }
